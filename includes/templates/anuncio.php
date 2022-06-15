8881361043444bdab86fc1370700e92152af80a1
Mario Valenzuela (wariomvc) <wario.mvc@gmai.com>
2022-06-15 01:06:23 -0500

    Fix para mostrar propiedades enindex

--- includes/templates/anuncio.php
+++ includes/templates/anuncio.php
@@ -1,41 +1,43 @@
 <?php 
 /* Iimportamos la conexion */
-require __DIR__ . '../../config/database.php';
+
+use App\Propiedad;
+
 $db = conectarBD();
 
 /* Consultar Propiedades */
+Propiedad::setDB($db);
+$propiedades = Propiedad::getAll();
 
-$query = "SELECT * FROM propiedades";
-$resultado = mysqli_query($db, $query);
 
 ?>
-<?php while($propiedad = mysqli_fetch_assoc($resultado)): ?>
+<?php foreach($propiedades as $propiedad): ?>
 <div class="anuncio">
     <picture>
         
-        <img src="/imagenes/<?php echo $propiedad['imagen'] ?>" alt="">
+        <img src="/imagenes/<?php echo $propiedad->imagen ?>" alt="">
     </picture>
     <div class="anuncio-contenido">
         <h3>Casa de Lujo</h3>
-        <p><?php echo $propiedad['descripcion']?></p>
-        <p class="anuncio-precio"><?php echo $propiedad['precio'] ?></p>
+        <p><?php echo $propiedad->descripcion?></p>
+        <p class="anuncio-precio"><?php echo $propiedad->precio ?></p>
         <ul class="iconos-caracteristicas">
             <li>
                 <img src="build/img/icono_dormitorio.svg" alt="">
-                <p><?php echo $propiedad['habitaciones']?></p>
+                <p><?php echo $propiedad->habitaciones?></p>
             </li>
             <li>
                 <img src="build/img/icono_estacionamiento.svg" alt="">
-                <p><?php echo $propiedad['estacionamiento']?></p>
+                <p><?php echo $propiedad->estacionamiento?></p>
             </li>
             <li>
                 <img src="build/img/icono_wc.svg " alt="">
-                <p><?php echo $propiedad['wc']?></p>
+                <p><?php echo $propiedad->wc?></p>
             </li>
         </ul>
-        <a href="propiedad.php?id=<?php echo $propiedad['id']; ?>" class="boton-amarillo-block">Detalles de Propiedad</a>
+        <a href="propiedad.php?id=<?php echo $propiedad->id; ?>" class="boton-amarillo-block">Detalles de Propiedad</a>
     </div>
 
 </div><!-- Anuncio -->
-<?php endwhile ?>
+<?php endforeach ?>
 <?php mysqli_close($db); ?>
